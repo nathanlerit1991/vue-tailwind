538b41789afddff8ce081271c24656f7f21898b9
nathanlerit1991 <nathan.lerit@gmail.com>
2024-03-27 10:56:00 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createMemoryHistory  } from 'vue-router'
 import { getCookie } from '@/helpers/getCookie'
 import HomePage from '../views/HomePage.vue'
 import Theme from '../views/Theme.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createMemoryHistory(),
   routes: [
     {
       path: '/',
