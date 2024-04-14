30a323bdb676a1cc9a8ebd6d1c010de690532dbb
nathanlerit1991 <nathan.lerit@gmail.com>
2024-04-14 23:44:44 +0800

    added password validation

--- src/views/Login/LoginPage.vue
+++ src/views/Login/LoginPage.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { ref } from 'vue'
+    import { ref, computed, onMounted } from 'vue'
     import { useRouter } from 'vue-router';
 
     import SectionWrapper from '@/components/SectionWrapper.vue'
@@ -14,8 +14,6 @@
     import Button from 'primevue/button';
 
     const router = useRouter();
-    const isRegister = ref(false)
-
     const loginFn = () => {
         var expires = ""
         let days = 30
@@ -26,6 +24,44 @@
 
         router.push('/');
     }
+
+    //Register
+    const isRegister = ref(false)
+    const register = ref(
+        {
+            password: ''
+        }
+    )
+    const passwordRules = {
+        minLength: ref(false),
+        hasUpperCase: ref(false),
+        hasLowerCase: ref(false),
+        hasSpecialChar: ref(false)
+    };
+    const validatePassword = (password) => {
+        checkMinLength(password);
+        checkUpperCase(password);
+        checkLowerCase(password);
+        checkSpecialChar(password);
+    };
+    //ADD / CHANGE BELOW CONDITIONS
+    const checkMinLength = (password) => {
+        passwordRules.minLength = password.length >= 8;
+    };
+    const checkUpperCase = (password) => {
+        passwordRules.hasUpperCase = /[A-Z]/.test(password);
+    };
+    const checkLowerCase = (password) => {
+        passwordRules.hasLowerCase = /[a-z]/.test(password);
+    };
+    const checkSpecialChar = (password) => {
+        passwordRules.hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    };
+
+
+onMounted(() => {
+    validatePassword(register.value.password);
+});
 </script>
 <template>
     <SectionWrapper id="login-wrapper" additional_class="h-full">
@@ -67,7 +103,21 @@
                             </div>
                             <div>
                                 <label>Password</label>
-                                <InputText class="block p-2 w-full" type="text" />
+                                <InputText v-model="register.password" class="block p-2 w-full" type="password" @input="validatePassword(register.password)" />
+                                <div class="password-rules">
+                                    <div>
+                                        <small>Must be 8 characters <span v-if="passwordRules.minLength">✔</span></small>
+                                    </div>
+                                    <div>
+                                        <small>Must have at least 1 Uppercase <span  v-if="passwordRules.hasUpperCase">✔</span></small>
+                                    </div>
+                                    <div>
+                                        <small>Must have at least 1 Lowercase <span v-if="passwordRules.hasLowerCase">✔</span></small>
+                                    </div>
+                                    <div>
+                                        <small>Must have at least 1 Special Character <span  v-if="passwordRules.hasSpecialChar">✔</span></small>
+                                    </div>
+                                </div>
                             </div>
                             <div>
                                 <label>Confirm Password</label>
